681e2ba5b3ffdbd61227ec4c2198f30761ccf135
shaadquazi <shaadquazi@gmail.com>
2022-09-04 14:01:25 -0400

    add flaskenv

--- src/server/app/__init__.py
+++ src/server/app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask, redirect, request, session
+import os
+
+client_id = os.environ.get('SPOTIFY_CLIENT_ID')
+client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
 app = Flask(__name__)
 
