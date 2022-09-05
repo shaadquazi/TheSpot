21c53c9fe2c13d60840a975925b27a8f1edef509
shaadquazi <shaadquazi@gmail.com>
2022-09-05 16:13:02 -0400

    adding CORS

--- src/server/application/__init__.py
+++ src/server/application/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, redirect, request, session
+from flask_cors import CORS
 import os
 
 client_id = os.environ.get('SPOTIFY_CLIENT_ID')
 client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
 application = Flask(__name__)
+CORS(application)
 
 from application import routes
