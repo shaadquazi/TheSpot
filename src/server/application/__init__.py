c46cf128c1bb9878442ec3b24e926e45c2a35033
shaadquazi <shaadquazi@gmail.com>
2022-09-08 00:14:49 -0400

    get base react url

--- src/server/application/__init__.py
+++ src/server/application/__init__.py
@@ -4,6 +4,12 @@ import os
 
 client_id = os.environ.get('SPOTIFY_CLIENT_ID')
 client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
+env = os.environ.get('FLASK_ENV')
+baseReactURL = ''
+if env == 'development':
+    baseReactURL = os.environ.get('REACT_APP_LOCAL_ENDPOINT')
+else:
+    baseReactURL = os.environ.get('REACT_APP_ENDPOINT')
 
 application = Flask(__name__)
 CORS(application)
